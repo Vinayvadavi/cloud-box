07f13237f2993a944b3d09dda0283e81e192c850
Vinayvadavi <137748631+Vinayvadavi@users.noreply.github.com>
2023-11-28 15:09:58 +0530

    Update temper.py

---
+++ temper.py
@@ -0,0 +1,7 @@
+What is YAML
+YAML ain't markup language. YAML is a concise data serialization language, which
+is typically considered easier to read than the more verbose JSON or XML. It is most often used in 
+configuration files of servers and software applications. Clark Evans proposed the spec in 2001 as 
+the result of an effort to simplify XML. According to his resume, 
+at the time he conceived of YAML he was working with Python, which likely 
+influenced the syntax of YAML as an indentation-based language.
